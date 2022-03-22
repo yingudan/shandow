e479f6eedc61036396c3e9d7f9d71cfe99f7abbc
Ripper <yingudan@vip.qq.com>
2022-03-22 21:28:36 +0800

    Initial commit

---
+++ src/main/java/com/fast/shadow/ShandowApplication.java
@@ -0,0 +1,13 @@
+package com.fast.shadow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShandowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShandowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fast/shadow/ShandowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fast.shadow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShandowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
